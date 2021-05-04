84444c7cf73182eebffeaae0b2b59e6419654a08
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-05-04 19:39:30 -0400

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,20 +5,23 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const goodFeedback = () => setGood(good + 1)
-  const neutralFeedback = () => setNeutral(neutral + 1)
-  const badFeedback = () => setBad(bad + 1)
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
   
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={goodFeedback}>good</button>
-      <button onClick={neutralFeedback}>neutral</button>
-      <button onClick={badFeedback}>bad</button>
+      <button onClick={incrementGood}>good</button>
+      <button onClick={incrementNeutral}>neutral</button>
+      <button onClick={incrementBad}>bad</button>
       <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good - bad)/(good + neutral + bad)}</div>
+      <div>percentage {100*good/(good + neutral + bad)}%</div>
     </div>
   )
 }
