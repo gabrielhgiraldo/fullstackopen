a6c9c0fe258f344fdf76aba41f108b54665590f2
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-05-05 10:45:41 -0400

    1.14*: anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [highestAnecdote, setHighestAnecdote] = useState(0)
 
   const getRandomAnecdote = () => {
     setSelected(Math.round(Math.random()*(anecdotes.length-1)))
@@ -19,18 +20,25 @@ const App = () => {
 
   const incrementVotes = () => {
     let votes_copy = [...votes]
-    votes_copy[selected]+=1
+    votes_copy[selected] += 1
+    if (votes_copy[selected] > votes_copy[highestAnecdote]){
+      setHighestAnecdote(selected)
+    }
     setVotes(votes_copy)
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <div>{anecdotes[selected]}</div>
       <div>has {votes[selected]} votes</div>
       <div>
         <button onClick={incrementVotes}>vote</button>
         <button onClick={getRandomAnecdote}>next anecdote</button>
       </div>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[highestAnecdote]}</div>
+      <div>has {votes[highestAnecdote]} votes</div>
     </div>
   )
 }
