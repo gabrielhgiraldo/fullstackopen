81e75b7efc3e38afe21363e1726b8964edb2d342
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-05-20 18:04:58 -0400

    4.5*: helper functions and unit tests, step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,10 +3,21 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = blogs => {
-    return blogs.reduce((likesSum, blog) => likesSum+=blog.likes, 0)
+    return blogs.reduce((likesSum, blog) => likesSum += blog.likes, 0)
 }
+
+const favoriteBlog = blogs => {
+    if (blogs.length == 1){
+        return blogs[0]
+    }
+    return blogs.reduce((currentFavorite, blog) => {
+        return blog.likes > currentFavorite.likes ? blog : currentFavorite
+    }, blogs.shift())
+}
+
 module.exports = {
     dummy,
+    favoriteBlog,
     totalLikes
 }
 
