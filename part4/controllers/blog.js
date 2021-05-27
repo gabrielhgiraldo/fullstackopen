01590aea2050601bec5b4e4b09df8a840a3d238c
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-05-27 18:05:11 -0400

    4.12*: Blog list tests, step5

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -2,23 +2,24 @@ const blogsRouter = require("express").Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs) 
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
-  
-blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog({
-      title: request.body.title,
-      author: request.body.author,
-      likes: request.body.likes || 0,
-      url: request.body.url,
-      date: new Date(),
-    })
 
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
+blogsRouter.post('/', async (request, response) => {
+  if (!request.body.url || !request.body.title) {
+    return response.status(400).send({error:'request missing url or title'})
+  }
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    likes: request.body.likes || 0,
+    url: request.body.url,
+    date: new Date(),
+  })
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
-  
-
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -5,53 +5,53 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const initialBlogs = [
     {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
     },
     {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
     },
     {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
     },
     {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
     },
     {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
     },
     {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
-    }  
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }
 ]
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -76,7 +76,7 @@ test('blog posts unique identifier is named id', async () => {
 test('new blog is created with post request', async () => {
     const newBlog = {
         title: 'i am a test blog, delete me.',
-        url:'https://testblog.com',
+        url: 'https://testblog.com',
         author: 'sample mcSamplesteen',
         likes: 0
     }
@@ -87,7 +87,7 @@ test('new blog is created with post request', async () => {
         .expect('Content-Type', /application\/json/)
 
     const response = await api.get('/api/blogs')
-    
+
     expect(response.body).toHaveLength(initialBlogs.length + 1)
 
     expect(response.body).toEqual(
@@ -100,7 +100,7 @@ test('new blog is created with post request', async () => {
 test('likes defaults to 0 if missing from request body on creation', async () => {
     const newBlog = {
         title: 'i am a test blog, delete me.',
-        url:'https://testblog.com',
+        url: 'https://testblog.com',
         author: 'sample mcSamplesteen',
     }
     await api
@@ -112,9 +112,35 @@ test('likes defaults to 0 if missing from request body on creation', async () =>
     const response = await api.get('/api/blogs')
     expect(response.body).toEqual(
         expect.arrayContaining([
-            expect.objectContaining({...newBlog, likes:0})
+            expect.objectContaining({
+                ...newBlog,
+                likes: 0
+            })
         ])
     )
+})
+
+test('blog creation without url or title returns 400', async () => {
+    const noURLBlog = {
+        title: 'i am a test blog, delete me.',
+        url: undefined,
+        author: 'sample mcSamplesteen',
+        likes: 0
+    }
+    const noTitleBlog = {
+        title: undefined,
+        url: 'https://testblog.com',
+        author: 'sample mcSamplesteen',
+        likes: 0
+    }
+    await api
+        .post('/api/blogs')
+        .send(noURLBlog)
+        .expect(400)
+    await api
+        .post('/api/blogs')
+        .send(noTitleBlog)
+        .expect(400)
 })
 afterAll(() => {
     mongoose.connection.close()
