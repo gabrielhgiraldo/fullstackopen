2f285e89ae3f8c163b3c14b9ecc6f5660f6cf55a
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-06-02 19:53:42 -0400

    4.17: bloglist expansion, step5

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,8 +1,9 @@
 const blogsRouter = require("express").Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {'username':1, 'name':1, 'id':1})
   response.json(blogs)
 })
 
@@ -10,15 +11,20 @@ blogsRouter.post('/', async (request, response) => {
   if (!request.body.url || !request.body.title) {
     return response.status(400).send({error:'request missing url or title'})
   }
+  const users = await User.find({})
+  const user = users[0]
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     likes: request.body.likes || 0,
     url: request.body.url,
     date: new Date(),
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
