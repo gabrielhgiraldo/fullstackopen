c91eb04961f47f5b17110de94bac24061e6fcd22
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-06-04 11:14:17 -0400

    4.22*: bloglist expansion, step10

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,30 +1,19 @@
-const jwt = require('jsonwebtoken')
 const blogsRouter = require("express").Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-
-const validateUser = async request => {
-  const token = request.body.token
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
-  return user
-}
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {'username':1, 'name':1, 'id':1})
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
   if (!body.url || !body.title) {
     return response.status(400).send({error:'request missing url or title'})
   }  
 
-  const user = await validateUser(request)
+  const user = request.body.user
 
   const blog = new Blog({
     title: request.body.title,
@@ -41,8 +30,8 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const user = await validateUser(request)
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+  const user = request.body.user
   const blog = await Blog.findById(request.params.id)
   if (blog.user.toString() !== user.id.toString()) {
     return response.status(403).json({ error: "only creator of blog can delete blog" })
