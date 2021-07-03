3af98d23608a9275c3212e796a9498b6493f859b
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-07-03 13:50:56 -0400

    6.13 Anecdotes and the backend, step1

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,9 +9,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch(action.type) {
@@ -33,6 +23,9 @@ const anecdoteReducer = (state = initialState, action) => {
       break
     case 'NEW_ANECDOTE':
       state = [...state, action.data]
+      break
+    case 'INITIALIZE':
+      state = action.data
       break
     default:
   }
@@ -50,6 +43,13 @@ export const createAnecdote = (content) => {
   return {
     type: 'NEW_ANECDOTE',
     data: asObject(content)
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INITIALIZE',
+    data: anecdotes
   }
 }
 
