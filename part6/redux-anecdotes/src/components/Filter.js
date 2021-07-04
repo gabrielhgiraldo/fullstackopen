a9527806234d6395443843345330ef52d50f9480
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-07-04 13:19:25 -0400

    6.20 anecdotes and connect, step2

--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { setFilter } from '../reducers/filterReducer'
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = props => {
   const handleChange = (event) => {
-    dispatch(setFilter(event.target.value))
+    props.setFilter(event.target.value)
   }
   const style = {
     marginBottom: 10
@@ -17,5 +16,6 @@ const Filter = () => {
     </div>
   )
 }
-
-export default Filter
+const mapDispatchToProps = {setFilter}
+const connectedFilter = connect(null, mapDispatchToProps)(Filter)
+export default connectedFilter
