b5e066b3355f7feaae0fa7ffd8174ae22fbb0696
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-07-25 16:43:01 -0400

    8.11: Authors birth year

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import { useMutation } from '@apollo/client'
+import React, { useState } from 'react'
+import { ALL_DATA, EDIT_AUTHOR } from '../queries'
 
 const Authors = ({ show, authors }) => {
+  const [ name, setName ] = useState('')
+  const [ born, setBorn ] = useState('')
+
+  const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_DATA } ]
+  })
+
+  const submit = (event) => {
+    event.preventDefault()
+    editAuthor({ variables: { name, setBornTo: parseInt(born) }})
+    setName('')
+    setBorn('')
+  }
+
   if (!show) {
     return null
   }
@@ -28,7 +44,24 @@ const Authors = ({ show, authors }) => {
           )}
         </tbody>
       </table>
-
+      <h3>set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name} 
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)
+          }/>
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
