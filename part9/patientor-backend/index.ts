ef2b4ffdc52a04f5ed66c04100193fb15e2fea31
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-08-10 15:14:45 -0400

    9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
-app.get('/ping', (_req, resp) => {
+app.get('/api/ping', (_req, resp) => {
     resp.send('pong')
 });
 
-const PORT = 3003
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
